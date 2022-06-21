24b62116327cbf5138d52275b0d1fe4145ef0ca0
EricPuah <ericpuah926@gmail.com>
2022-06-21 16:17:50 +0800

    user

--- Project classes/student.hpp
+++ Project classes/student.hpp
@@ -2,30 +2,35 @@
 #define student_h
 #include "user.hpp"
 #include<iostream>
+#include<typeinfo>
 using namespace std;
 
 class student : public user
 {
     private:
-    string name, matricNo;
+    string name, ID;
     int age;
     double height,weight;
-    Sport* sp;
+   // Sport* sp;
     
     public:
-    student(string name, int age, double weight, double height, string ID)
+    student():user(ID)
+    {
+        inputStudent();
+    };
+    student(string ID, string name,int age, double weight, double height): user(ID)
     {
         this->name = name;
         this->age = age;
         this->weight = weight;
         this->height = height;
-        this->ID = matricNo;
+        this->ID = ID;
     } 
     string getID()
     {
-        return matricNo;
+        return ID;
     }
-    Report getReport();
+   // Report getReport();
     string getName()
     {
         return name;
@@ -42,7 +47,104 @@ class student : public user
     {
         return weight;
     }
+    void display()
+    {
+        cout<<name<<endl;
+        cout<<ID<<endl;
+        cout<<age<<endl;
+        cout<<weight<<endl;
+        cout<<height<<endl;
+    }
+    void inputStudent()
+    {
+        do
+        {
+            cout<<"Enter your name: ";
+            try
+            {
+                getline(cin,name);
+                if (typeid(name) != typeid(string))
+                {
+                    throw "tryagain";
+                }
+            }
+            catch(int)
+            {
+                cout<<"Invalid name, please try again";
+            }
+        } while (typeid(name) != typeid(string));
+        
+        do
+        {
+            cout<<"Enter your ID: ";
+            try
+            {
+                getline(cin,ID);
+                if (!cin)
+                {
+                    throw "tryagain";
+                }
+            }
+            catch(...)
+            {
+                cout<<"Invalid ID, please try again";
+            }
+        } while (typeid(ID) != typeid(string));
+        
+        do
+        {
+            cout<<"Enter your age: ";
+            cin>>age;
+            try 
+            {
+                if (typeid(age) != typeid(int))
+                {
+                    throw "tryagain";
+                }
+            }
+            catch(...)
+            {
+                cout<<"Invalid age, please try again";
+            }
+        } while (typeid(age) != typeid(int));
+
+        do
+        {
+            cout<<"Enter your weight: ";
+            cin>>weight;
+            try 
+            {
+                if (typeid(weight) != typeid(double))
+                {
+                    throw "tryagain";
+                }
+            }
+            catch(...)
+            {
+                cout<<"Invalid weight, please try again";
+            }
+        } while (typeid(weight) != typeid(double));  
+
+        do
+        {
+            cout<<"Enter your weight: ";
+            cin>>height;
+            try 
+            {
+                if (typeid(height) != typeid(double))
+                {
+                    throw "tryagain";
+                }
+            }
+            catch(...)
+            {
+                cout<<"Invalid height, please try again";
+            }
+        } while (typeid(height) != typeid(double));  
+    }
+
 };
+
 
 
 #endif
